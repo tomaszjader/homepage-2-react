9ffb8f2266672db942fc64e4e0dfe87fd73a53d1
Tomasz Jąder <tomasz.jader@gmail.com>
2024-12-17 23:51:40 +0100

    add InfoSection HTML, CSS and assets

--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './InfoSection.css';
 
 interface InfoSectionProps {
   // props here
@@ -6,8 +7,30 @@ interface InfoSectionProps {
 
 export const InfoSection: React.FC<InfoSectionProps> = () => {
   return (
-    <div>
-      InfoSection Component
-    </div>
+    <section id="about">
+      <div className="about">
+        <div className="about-text">
+          <h1 className="about-header">My name is Tomasz and I’m a <span className="about-header-custom">Junior Fullstack developer</span> and AI fan.</h1>
+
+          <p className="about-pargrph">You might be interested in checking the list of my <a href="#skills"
+            className="about-pargrph-custom about-link">skills</a>. Don’t forget to have a
+            glimpse at my <a href="#projects" className="about-pargrph-custom about-link">projects</a>. I’m looking for a new challenges so if
+            you have one - <a href="#contact" className="about-pargrph-custom about-link">contact me</a> 😊</p>
+          <div className="buttons">
+            <a href="#contact" className="about-button-contact about-link link-font">
+              <img src="assets/img/cloud.svg" alt="" height="24" />
+              Get in touch
+            </a>
+            <a className="about-button-know about-link link-font" href="#skills" >
+              <img src="assets/img/image-user.svg" alt="" height="24" />
+              Get to know me
+            </a>
+          </div>
+
+        </div>
+
+        <img src="assets/img/47568523.jpg" alt="" className="about-img" height="400" />
+      </div>
+    </section>
   );
 };
