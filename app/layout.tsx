56089c03f701ecf3d600eefe83240f9e30a84b5c
Tomasz Jąder <tomasz.jader@gmail.com>
2024-12-23 21:06:04 +0100

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,19 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Homepage 2 react/next.js",
-  description: "Homepage 2 react/next.js",
+  description: "Explore my personal portfolio showcasing my skills, projects, and expertise. Discover who I am, what I do, and how I can contribute to your team or project.",
+  openGraph: {
+    title: "Homepage 2 react/next.js",
+    description: "Explore my personal portfolio showcasing my skills, projects, and expertise. Discover who I am, what I do, and how I can contribute to your team or project.",
+    url: "https://tomaszjader.github.io/homepage-2-react/",
+    siteName: "Homepage 2 react/next.js",
+    images: [
+      {
+        url: "https://tomaszjader.github.io/homepage-2-react/assets/img/open_graph_img.png",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
