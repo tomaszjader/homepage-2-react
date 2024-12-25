00e4f8bd77c8f66d8fc3d9a7a893b90cc421ac01
Tomasz Jąder <tomasz.jader@gmail.com>
2024-12-25 13:07:45 +0100

    add BEM to AfterHouersSection

--- src/components/AfterHoursSection/AfterHoursSection.tsx
+++ src/components/AfterHoursSection/AfterHoursSection.tsx
@@ -10,72 +10,76 @@ interface AfterHoursSectionProps {
 
 export const AfterHoursSection: React.FC<AfterHoursSectionProps> = () => {
   return (
-    <div className="after-houer">
+    <div className="after-hours">
       <div>
-        <div className="after-houer-discription">
-          <h2 className="after-houer-header">After Hours</h2>
-          <p className="after-houer-p">
-            Outside of my regular working hours, I delve into an array of activities
-            and passions. Here&apos;s a glimpse into how I spend my after-hours.
-          </p>
-          <div className="after-houers">
-            <div className="image-sections">
-              <div className="image-section">
+        <div className="after-hours__content">
+          <div className="after-hours__header-container">
+            <h2 className="after-hours__header">After Hours</h2>
+            <p className="after-hours__text">
+              Outside of my regular working hours, I delve into an array of activities
+              and passions. Here&apos;s a glimpse into how I spend my after-hours.
+            </p>
+          </div>
+
+          <div className="after-hours__grid">
+            <div className="after-hours__row">
+              <div className="card">
                 <Image
+                  className="card__image"
                   src={getImagePath("assets/img/ai.jpg")}
                   alt=""
                   width={320}
                   height={480}
-                  style={{ borderRadius: '20px' }}
                 />
-                <h4 className="image-header">Artificial Intelligence</h4>
-                <p className="image-p">
+                <h4 className="card__header">Artificial Intelligence</h4>
+                <p className="card__text">
                   After work I test LLM and automate my work
                 </p>
               </div>
-              <div className="image-section">
+              <div className="card">
                 <Image
+                  className="card__image"
                   src={getImagePath("assets/img/travel.jpg")}
                   alt=""
                   width={320}
                   height={480}
-                  style={{ borderRadius: '20px' }}
                 />
-                <h4 className="image-header">Travel</h4>
-                <p className="image-p">
+                <h4 className="card__header">Travel</h4>
+                <p className="card__text">
                   In my spare time in love with foreign travel and new adventures
                 </p>
               </div>
             </div>
-            <div className="image-sections">
-              <div className="image-section">
-                <Image 
-                  src={getImagePath("assets/img/games.png")} 
-                  alt="" 
-                  width={316} 
-                  height={480} 
+            <div className="after-hours__row">
+              <div className="card">
+                <Image
+                  className="card__image"
+                  src={getImagePath("assets/img/games.png")}
+                  alt=""
+                  width={316}
+                  height={480}
                 />
-                <h4 className="image-header">Video Games</h4>
-                <p className="image-p">
+                <h4 className="card__header">Video Games</h4>
+                <p className="card__text">
                   Lost in virtual realms, I find joy and challenge in the dynamic
                   world of video games.
                 </p>
               </div>
-              <div className="image-section">
-                <Image 
-                  src={getImagePath("assets/img/gim.png")} 
-                  alt="" 
-                  width={316} 
-                  height={480} 
+              <div className="card">
+                <Image
+                  className="card__image"
+                  src={getImagePath("assets/img/gim.png")}
+                  alt=""
+                  width={316}
+                  height={480}
                 />
-                <h4 className="image-header">Strength Training</h4>
-                <p className="image-p">
+                <h4 className="card__header">Strength Training</h4>
+                <p className="card__text">
                   Empowering both body and mind, I&apos;m dedicated to the discipline of
                   strength training, continuously pushing my limits.
                 </p>
               </div>
             </div>
-
           </div>
         </div>
       </div>
