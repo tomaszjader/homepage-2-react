c5a74aaef4218d48b4f508ca4d9c0b6545293f2b
Tomasz Jąder <tomasz.jader@gmail.com>
2025-05-24 23:47:12 +0200

    added Tailwind css to SkillsSection

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { SkillsItem } from '@/src/components/SkillsItem/SkillsItem';
-import './SkillsSection.css';
-
 
 export const SkillsSection: React.FC = () => {
   const workingKnowledgeArray: string[] = ['— Semantic HTML5', '— Flexbox',
@@ -13,32 +11,31 @@ export const SkillsSection: React.FC = () => {
   const wantToLearnArray: string[] = ['— Node JS', '— Electron',
     '— Server side rendering', '— Gatsby', '— Websockets'];
   return (
-    
-<section className="skills" id="skills">
-    <article>
-        <div className="skills__content">
-            <h2 className="skills__content-header">MY SKILLS</h2>
-            <h3 className="skills__content-paragraf">I&apos;ve picked up some cool skills along the way, and I&apos;m excited to share them with you. Check out what I can do below!</h3> 
+    <section className="flex items-center justify-center flex-col my-[120px]" id="skills">
+      <article>
+        <div className="md:mx-6">
+          <h2 className="text-[20px] text-gray-400 ml-auto">MY SKILLS</h2>
+          <h3 className="text-white text-[40px] font-medium w-auto md:w-[864px] mb-20">I&apos;ve picked up some cool skills along the way, and I&apos;m excited to share them with you. Check out what I can do below!</h3> 
         </div>
         
-        <div className  ="skills__boxs">
-            <SkillsItem
-            skillsArray = {workingKnowledgeArray}
-            skillsHeader = "Working knowledge"
-            skillsImage = "assets/img/trangle.svg"
-            ></SkillsItem>
-            <SkillsItem
-            skillsArray = {knowSomethingAboutArray}
-            skillsHeader = "Know something about"
-            skillsImage = "assets/img/elicpse.svg"
-            ></SkillsItem>
-            <SkillsItem    
-            skillsArray = {wantToLearnArray}
-            skillsHeader = "Want to learn"
-            skillsImage = "assets/img/test.svg"
-            ></SkillsItem>
+        <div className="flex justify-center items-center gap-4 md:flex-row flex-col">
+          <SkillsItem
+            skillsArray={workingKnowledgeArray}
+            skillsHeader="Working knowledge"
+            skillsImage="assets/img/trangle.svg"
+          />
+          <SkillsItem
+            skillsArray={knowSomethingAboutArray}
+            skillsHeader="Know something about"
+            skillsImage="assets/img/elicpse.svg"
+          />
+          <SkillsItem    
+            skillsArray={wantToLearnArray}
+            skillsHeader="Want to learn"
+            skillsImage="assets/img/test.svg"
+          />
         </div>
-    </article>
-</section>
+      </article>
+    </section>
   );
 };
